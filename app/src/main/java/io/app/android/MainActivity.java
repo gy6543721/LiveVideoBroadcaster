7227bcdae3c23756f62b22fa6e164c36c64c53ba
gy6543721 <gy6543721@yahoo.com.tw>
2022-04-26 10:41:21 +0900

    Initial commit

---
+++ app/src/main/java/io/app/android/MainActivity.java
@@ -0,0 +1,33 @@
+package io.app.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import io.app.android.liveVideoBroadcaster.*;
+import io.app.android.liveVideoPlayer.LiveVideoPlayerActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * PLEASE WRITE RTMP BASE URL of the your RTMP SERVER.
+     */
+    public static final String RTMP_BASE_URL = "rtmp://10.10.31.87/LiveApp/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(io.app.android.liveVideoBroadcaster.R.layout.activity_main);
+    }
+
+    public void openVideoBroadcaster(View view) {
+        Intent i = new Intent(this, LiveVideoBroadcasterActivity.class);
+        startActivity(i);
+    }
+
+    public void openVideoPlayer(View view) {
+        Intent i = new Intent(this, LiveVideoPlayerActivity.class);
+        startActivity(i);
+    }
+}
